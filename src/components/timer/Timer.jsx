0f994599fde8aec6a2d79566da2a792e7b93eb54
Vasco Oliveira <vascooliveira@outlook.com>
2021-01-10 06:03:50 +0000

    added default Timer

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './Timer.css';
 
-  const Timer = (props) => {
-  return <h1>Hello, {props.name}</h1>;
-  }
+const Timer = (props) => {
+  
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(setTime(new Date()), 1000);
+
+    // Effect Cleanup Function
+    return () => clearInterval(intervalId);
+  }, [time]);
+
+  return (
+    <h1>{time.toLocaleTimeString()}</h1>
+  );
+}
 
 export default Timer;
